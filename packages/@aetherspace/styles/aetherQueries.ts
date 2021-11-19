ee400994876c5e02447d28a5b4a0a0bdbcaaf507
codinsonn <thor_r_stevens@hotmail.com>
2021-11-19 21:36:57 +0100

    Add aetherspace utils

--- packages/@aetherspace/styles/aetherQueries.ts
+++ packages/@aetherspace/styles/aetherQueries.ts
@@ -2,13 +2,13 @@
 import { camelToDash, createKey } from '../utils';
 
 /* --- SSR Media Queries ----------------------------------------------------------------------- */
-
 // -i- Loosely based on: https://gist.github.com/EyMaddis/35ae3b269e4658527a1f8e374bd434ac#file-lib_cssinjection-ts
+
 const mediaQueries: { [id: string]: string } = {};
 const AETHER_QUERIES = 'AetherQueries';
 
 /* --- getUnit() ------------------------------------------------------------------------------- */
-
+// -i- normalize units for use cross-platform
 const getUnit = (classKey: string) => {
     let unit = '';
     if (['margin', 'padding'].some((cssKey) => classKey.includes(cssKey))) unit = 'px';
@@ -16,7 +16,7 @@ const getUnit = (classKey: string) => {
 };
 
 /* --- addMediaQuery() ------------------------------------------------------------------------- */
-
+// -i- adds a media query to the in memory style object
 export const addMediaQuery = (breakpoint: number, styles: Object): string => {
     const styleId = `${breakpoint}-${createKey(styles)}`; // @ts-ignore
     // Build CSS rules from style object
@@ -32,7 +32,7 @@ export const addMediaQuery = (breakpoint: number, styles: Object): string => {
 };
 
 /* --- getInjectables() ------------------------------------------------------------------------ */
-
+// -i- fetch all server-rendered styles
 export const getInjectables = () => ({
     id: AETHER_QUERIES,
     css: Object.values(mediaQueries).join('\n'),
